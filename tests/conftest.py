b8490efd586b70e3b9fc375cee8ab21e26af043b
SouheilMaatoug <souheil.maatoug@gmail.com>
2026-01-18 18:06:32 +0100

    feat: add audio fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 from typing import Tuple
-
+import soundfile as sf
 import cv2
 import numpy as np
 import pytest
+import subprocess
 
 
 @pytest.fixture(scope="session")
@@ -15,7 +16,19 @@ def fps() -> float:
 @pytest.fixture(scope="session")
 def n_frames() -> int:
     """Number of frames."""
-    return 12
+    return 50
+
+
+@pytest.fixture(scope="session")
+def duration(fps, n_frames) -> float:
+    """Duration (s)."""
+    return float(fps * n_frames)
+
+
+@pytest.fixture(scope="session")
+def sample_rate() -> int:
+    """Sample rate (Hz)."""
+    return int(16e3)
 
 
 @pytest.fixture(scope="session")
@@ -39,7 +52,7 @@ def make_frame(size):
     return _make
 
 @pytest.fixture(scope="function")
-def make_video(tmp_path: Path, codec, size, fps, n_frames, make_frame):
+def make_video_file(tmp_path: Path, codec, size, fps, n_frames, make_frame):
     """Create a synthetic video in a temporary file."""
     filename = "sample.mp4"
     output_path = tmp_path / filename
@@ -49,4 +62,38 @@ def make_video(tmp_path: Path, codec, size, fps, n_frames, make_frame):
         frame = make_frame()
         writer.write(frame)
     writer.release()
+    return output_path
+
+@pytest.fixture(scope="function")
+def make_audio(sample_rate, duration):
+    """Generate a synthetic audio track."""
+    n_samples = int(round(sample_rate * duration))
+    data = (np.random.rand(n_samples) * 2) - 1
+    return data
+
+@pytest.fixture(scope="function")
+def make_audio_file(tmp_path: Path, sample_rate, make_audio):
+    """Create a synthetic audio in a temporary file."""
+    filename = "sample.wav"
+    output_path = tmp_path / filename
+    data = make_audio
+    sf.write(str(output_path), data, sample_rate) 
+    return output_path
+        
+@pytest.fixture(scope="function")
+def make_audio_video_file(tmp_path: Path, make_video_file, make_audio_file):
+    """Create a synthetic video audio in a temporary file."""
+    video_file = make_video_file
+    audio_file = make_audio_file
+    filename = "sample.mp4"
+    output_path = tmp_path / filename
+    cmd = [
+        "ffmpeg", "-y",
+        "-i", str(video_file),
+        "-i", str(audio_file),
+        "-c:v", "copy",
+        "-c:a", "aac",
+        str(output_path)
+    ]
+    _ = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.Pipe)
     return output_path
