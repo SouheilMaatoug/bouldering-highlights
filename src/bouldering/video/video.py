63391d4ae80d14152611c9261d9ac2a384c324c4
SouheilMaatoug <souheil.maatoug@gmail.com>
2026-01-18 14:18:54 +0100

    feat: add video class

---
+++ src/bouldering/video/video.py
@@ -0,0 +1,47 @@
+from src.bouldering.video.audio import Audio
+from src.bouldering.video.sequence import Sequence
+from src.bouldering.video.video_reader import VideoReader
+from src.bouldering.video.video_writer import VideoWriter
+
+
+class Video:
+    """Class for Video instance."""
+
+    def __init__(self, sequence: Sequence, audio: Audio):
+        self.sequence = sequence
+        self.audio = audio
+
+    @classmethod
+    def read(cls, path: str) -> "Video":
+        with VideoReader(path) as vr:
+            sequence = vr.extract_sequence()
+            audio = vr.extract_audio()
+            return cls(sequence, audio)
+
+    @property
+    def metadata(self) -> dict:
+        return {
+            "video": {
+                "fps": self.sequence.fps,
+                "n_frames": self.sequence.n_frames,
+                "resolution": self.sequence.resolution,
+            },
+            "audio": {"sample_rate": self.audio.sample_rate, "duration": self.audio.duration},
+        }
+
+    @staticmethod
+    def time2frame(time: float, fps: float):
+        """Convert timestamp to frame number."""
+        return int(round(time * fps))
+
+    def cut(self, start_time: float, end_time) -> "Video":
+        fps = self.metadata["video"]["fps"]
+        start_frame = self.time2frame(start_time, fps)
+        end_frame = self.time2frame(end_time, fps)
+        sequence_cut = self.sequence.cut(start_frame, end_frame)
+        audio_cut = self.audio.cut(start_time, end_time)
+        return Video(sequence_cut, audio_cut)
+
+    def write(self, output_path: str) -> None:
+        with VideoWriter(output_path) as vw:
+            vw.write(self.sequence, self.audio)
