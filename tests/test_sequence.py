99fb3fe2c93f38fa13c45e9ac9a18520270be071
SouheilMaatoug <souheil.maatoug@gmail.com>
2026-01-18 18:07:05 +0100

    feat: add unit test for sequence module

---
+++ tests/test_sequence.py
@@ -0,0 +1,19 @@
+from src.bouldering.media.video.sequence import Sequence
+
+def test_sequence(make_video_file, fps, size, n_frames):
+    """Test the class Sequence."""
+    video_file = make_video_file
+    seq = Sequence(str(video_file), fps=fps, resolution=size, start=0, end=n_frames)
+
+    # metadata
+    assert seq.fps == fps
+    assert seq.resolution == size
+    assert seq.start == 0
+    assert seq.n_frames == n_frames
+
+    # cut sequence
+    seq_cut = seq.cut(20, 25)
+    assert seq_cut.fps == seq.fps
+    assert seq_cut.resolution == seq.resolution
+    assert seq_cut.n_frames == 5
+    
